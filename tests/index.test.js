15caa58189c48e29e1aa2d3d0e23e88fce65af16
Thierry PAGES <thipages@gmail.com>
2023-07-25 00:45:37 +0200

    tests : add subscrption test

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
 import oneStop from './../src/index.js'
+import {jest} from '@jest/globals'
 const initialState = {
   count:0,
   array : [1, 2, 3],
@@ -9,17 +10,22 @@ const initialState = {
     this.count += inc
   }
 }
+const mockCallback = jest.fn(x => true);
 let shop
 beforeEach(() => {
   shop = oneStop(initialState)
+  
 });
 describe("Check set/get", () => {
   it('should update a non-nested primitive', () => {
     shop.set.increment(2)
     expect(shop.get.count).toBe(2)
+
   })
   it('should update an array', () => {
+    shop.subscribe(mockCallback)
     shop.set.array.push(4)
     expect(shop.get.array.length).toBe(4)
+    expect(mockCallback.mock.calls).toHaveLength(2);
   })
  })
